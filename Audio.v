593e29069de0298170ec4a7d727f78b883fa4852
Ross MacArthur <macarthur.ross@gmail.com>
2016-07-21 15:29:26 +0200

    Now generates sine wave - Uses a full sine wave LUT with 256 samples and 8 bit resolution - Sample frequency can be adjusted with switches

--- Audio.v
+++ Audio.v
@@ -1,14 +1,14 @@
 module Audio(
     input clk,
-    input [10:0] value,
+    input [7:0] value,
     output reg AUD_PWM
 );
 
-reg [10:0] count;
+reg [7:0] count;
 
 always @(posedge clk) begin
     count <= count + 1'b1;
-    AUD_PWM <= value < count ? 1'bz : 1'b0;
+    AUD_PWM <= (value < count) ? 1'bz : 1'b0;
 end
 
 endmodule
--- Main.v
+++ Main.v
@@ -1,24 +1,32 @@
 module Main(
     input CLK_100M,
+    input [15:0] SW,
     output AUD_PWM,
-    output reg LED,
     output reg AUD_SD=1'b1
 );
 
-// Create a square wave of frequency 500 Hz
-reg [10:0] value;
-reg [17:0] count;
+// Count phase for sine wave
+wire [7:0] value;
+reg [7:0] phase;
+reg [15:0] count;
 always @(posedge CLK_100M) begin
-    count <= count + 1'b1;
-    LED <= count < 18'h1FFFF ? 1'b1 : 1'b0;
-    value <= count < 18'h1FFFF ? 11'h400 : 18'h0;
+    if (count == SW) begin
+        count <= 1'b0;
+        phase <= phase + 1'b1;
+    end else
+        count <= count + 1'b1;
 end
 
 // Connect up modules
-Audio AudioJack(
+Audio Audio0(
     .clk     ( CLK_100M ), // input
-    .value   ( value    ), // input [11:0]
+    .value   ( value    ), // input [7:0]
     .AUD_PWM ( AUD_PWM  )  // output
+);
+
+PAC PAC0 (
+    .phase     ( phase ), // input [7:0]
+    .amplitude ( value )  // output [7:0]
 );
 
 endmodule
