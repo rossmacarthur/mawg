5fc747bfbd95418ccf3f53d2293a8f799916e077
Ross MacArthur <macarthur.ross@gmail.com>
2016-07-22 13:05:22 +0200

    Sine wave is now generated with a quarter of the LUP - Wave generator script has option to generate full wave still

--- scripts/wave-generator.py
+++ scripts/wave-generator.py
@@ -9,9 +9,10 @@ import click
 @click.argument('samples', type=click.IntRange(1, 16))
 @click.argument('resolution', type=click.IntRange(1, 128))
 @click.option('--padding', '-p', type=int, default=0)
-def main(samples, resolution, padding):
+@click.option('--full', '-f', is_flag=True)
+def main(samples, resolution, padding, full):
     """
-    Generates a full sine wave in positive integer values between zero and a max resolution value.
+    Generates a sine wave in positive integer values between zero and a max resolution value.
 
     SAMPLES is the number of bits to use to represent the number of samples.
     RESOLUTION is the number of bits to represent the amount of resolution.
@@ -20,10 +21,12 @@ def main(samples, resolution, padding):
     S = 2**samples
     R = 2**resolution-1
 
-    for s in range(S):
+    rng = S if full else S//4
+
+    for s in range(rng):
         v = (sin(2*pi*s/S)+1)/2
-        print " "*padding + "{:d}'h{:0{}X} : amplitude <= {:d}'h{:0{}X};".format(
-            samples, s, (samples+3)//4, resolution, int(R*v), (resolution+3)//4)
+        print " "*padding + "{:d}'h{:0{}X} : value <= {:d}'h{:0{}X};".format(
+            samples-2, s, (samples+1)//4, resolution, int(R*v+.5), (resolution+3)//4)
 
 if __name__ == "__main__":
     main()
