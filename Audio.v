ad30697e2cd20483f66b12f586ff6c221c1e5153
Ross MacArthur <macarthur.ross@gmail.com>
2016-08-29 10:55:07 +0200

    NCO Version 0.2 - Clock divided - Signed 8-bit output - 256 samples

--- Audio.v
+++ Audio.v
@@ -8,7 +8,7 @@ reg [7:0] count;
 
 always @(posedge clk) begin
     count <= count + 1'b1;
-    AUD_PWM <= (value < count) ? 1'bz : 1'b0;
+    AUD_PWM <= ({~value[7], value[6:0]} < count) ? 1'bz : 1'b0;
 end
 
 endmodule
