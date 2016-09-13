e90fd75464e5b5d0860a62e1b8428f80a837e500
Ross MacArthur <macarthur.ross@gmail.com>
2016-09-13 08:38:38 +0200

    Got frequency modulation working - Uses one NCO to generate a modulating wave - Uses one NCO for the carrier

--- Main.v
+++ Main.v
@@ -5,29 +5,42 @@ module Main (
     output reg AUD_SD=1'b1
 );
 
-wire [7:0] value;
-wire [31:0] control;
-assign control = SW[13:4] << SW[3:0];
+// Modulate
+wire [7:0] message;
+wire [7:0] modulated;
+reg [31:0] carrier_ctrl;
+reg [7:0] umessage;
+reg is_negative;
+always @(*) begin
+    carrier_ctrl <= is_negative ? 32'h1F751 - {umessage,8'b0} : 32'h1F751 + {umessage,8'b0};
+    umessage <= message[7] ? ~message[7:0]+1'b1 : message[7:0];
+    is_negative <= message[7];
+end
+
+// Select output
+wire [7:0] to_audio;
+assign to_audio = SW[15] ? modulated : message;
 
 // Connect up modules
-WaveGenerator WaveGenerator0 (
-    .clk              ( CLK_100M     ), // input
-    .wave_sel         ( SW[15:14]    ), // input [1:0]
-    .freq_ctrl        ( 32'h49D2     ), // input [31:0]
-    .chirp_is_down    ( SW[13]       ), // input
-    .chirp_delay      ( SW[3:0]      ), // input [3:0]
-    .chirp_min_ctrl   ( 32'h10C7     ), // input [31:0]
-    .chirp_max_ctrl   ( 32'h68DB9    ), // input [31:0]
-    .chirp_inc_rate   ( 32'h355      ), // input [31:0]
-    .chirp_div_rate   ( 32'hB7AA18   ), // input [31:0]
-    .pulse_duty_cycle ( 32'h3FFFFFFF ), // input [31:0]
-    .wave_out         ( value        )  // output [7:0]
-//  .wave2_out        (              )  // output [7:0]
+NCO NCO0 (
+    .clk     ( CLK_100M ), // input
+    .reset   ( 1'b0     ), // input
+    .ctrl    ( 32'h10C7 ), // input [31:0]
+    .sin_out ( message  )  // output [7:0]
+    //.cos_out ( cos_out )  // output [7:0]
+);
+
+NCO NCO1 (
+    .clk     ( CLK_100M     ), // input
+    .reset   ( 1'b0         ), // input
+    .ctrl    ( carrier_ctrl ), // input [31:0]
+    .sin_out ( modulated    )  // output [7:0]
+    //.cos_out ( cos_out )  // output [7:0]
 );
 
 Audio Audio0 (
     .clk     ( CLK_100M ), // input
-    .value   ( value    ), // input [7:0]
+    .value   ( to_audio ), // input [3:0]
     .AUD_PWM ( AUD_PWM  )  // output
 );
 
--- scripts/control.py
+++ scripts/control.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python
 
-from __future__ import division
+from __future__ import division, print_function
 import click
+
+RESETCLR = '\033[0m'
+FADEDCLR = '\033[33m'
+THEMECLR = '\033[95m'
+WARNCLR = '\033[90m'
+
+
+class BasedIntParamType(click.ParamType):
+    name = 'integer'
+
+    def convert(self, value, param, ctx):
+        try:
+            if value[:2].lower() == '0x':
+                return int(value[2:], 16)
+            elif value[:2].lower() == '0b':
+                return int(value[2:], 2)
+            elif value[:1] == '0':
+                return int(value, 8)
+            return int(value, 10)
+        except ValueError:
+            self.fail('%s is not a valid integer' % value, param, ctx)
+
+BASED_INT = BasedIntParamType()
 
 
 def mod(x):
@@ -11,85 +34,164 @@ def mod(x):
         return -1 * x
 
 
+def print_theme(x):
+    print(THEMECLR, '\n', x, RESETCLR, sep='')
+
+
+def print_faded(x):
+    print(FADEDCLR, x, RESETCLR, sep='')
+
+
+def print_warning(x):
+    print(WARNCLR, x, RESETCLR, sep='')
+
+
 @click.group()
 def cli():
     pass
 
 
 @cli.command()
+@click.argument('center_freq', type=float)
+@click.option('--clk', '-c', help='Set the clock value in Hz (default 100 MHz).', default=100000000)
+def fm_modulation(center_freq, clk):
+    center_ctrl = int(round(center_freq * 2**32 / clk))
+    min_freq = (center_ctrl-(127 << 8)) * clk / 2**32
+    max_freq = (center_ctrl+(127 << 8)) * clk / 2**32
+    print('  center_ctrl: 0x{:X}'.format(center_ctrl))
+    print('  min_freq:    {} Hz'.format(min_freq))
+    print('  max_freq:    {} Hz'.format(max_freq))
+
+
+@cli.command()
 @click.argument('min_freq', type=float)
 @click.argument('max_freq', type=float)
 @click.argument('length', type=float)
-@click.option('--clk', '-c', default=100000000)
-def chirp(min_freq, max_freq, length, clk):
-    print '\nCHIRP SPECIFICATIONS:\n'
-    print '  min freq: {} Hz'.format(min_freq)
-    print '  max freq: {} Hz'.format(max_freq)
-    print '  length:   {} s'.format(length)
+@click.option('--clk', '-c', help='Set the clock value in Hz (default 100 MHz).', default=100000000)
+@click.option('--verilog', '-v', help='Prints out Verilog instantiation code.', is_flag=True)
+def chirp(min_freq, max_freq, length, clk, verilog):
+    """
+    Calculates input port values for the Chirp module.
 
-    print '\nCHIRP MODULE REQUIREMENTS:'
+    MIN_FREQ is the minimum frequency of the sweep.\t\t\t\t
+    MAX_FREQ is the maximum frequency of the sweep.\t\t\t\t
+    LENGTH is the time taken to sweep.
+    """
+
+    print_theme('Chirp module output specifications:')
+    print('  Minimum frequency of sweep: {} Hz'.format(min_freq))
+    print('  Maximum frequency of sweep: {} Hz'.format(max_freq))
+    print('  Length of sweep:            {} s'.format(length))
+
+    print_theme('Chirp module input ports:')
 
     min_ctrl = int(round(min_freq * 2**32 / clk))
     max_ctrl = int(round(max_freq * 2**32 / clk))
 
-    if not 2**32 > min_ctrl >= 1:
-        print '  Unfortunately no min_ctrl word can satisfy these specifications.'
-        return
-    if not 2**32 > max_ctrl >= 1:
-        print '  Unfortunately no max_ctrl word can satisfy these specifications.'
-        return
-
     best = 0, 0
     c = (length * clk) / (max_ctrl - min_ctrl)  # d/i
+    d, i = int(round(c)), 1
 
-    d = int(round(c))
-    i = 1
-
-    print '\033[33m'
-    print '  minimize inc_rate for a smoother frequency sweep'
-    print '  maximize div_rate for better chirp length accuracy'
-    print
-    print '  div_rate / inc_rate needs to be {}'.format(c)
-    print
-    print '  {} => 0x{:X} / 0x{:X} = {}'.format(c, d, i, d/i)
+    print_faded('  minimize inc_rate for a smoother frequency sweep')
+    print_faded('  maximize div_rate for better chirp length accuracy\n')
+    print_faded('  div_rate / inc_rate needs to be {}\n'.format(c))
+    print_faded('  0x{:X} / 0x{:X} = {}'.format(d, i, d/i))
     best = d, i
-    while d < 2**32:
-        d += 1
-        i = int(round(d/c))
+    while d < 2**32 and i < 2**32:
+        i += 1
+        d = int(round(c * i))
         e_best = mod(best[0]/best[1] - c)
         e_now = mod(d/i - c)
         if e_now < e_best:
             best = d, i
-            print '  {} => 0x{:X} / 0x{:X} = {}'.format(c, d, i, d/i)
+            print_faded('  0x{:X} / 0x{:X} = {}'.format(d, i, d/i))
         if e_best < 10**(-6):
+            print()
             break
-    print '\033[0m'
-    print '  min_ctrl: 0x{:X}'.format(min_ctrl, min_ctrl)
-    print '  max_ctrl: 0x{:X}'.format(max_ctrl, max_ctrl)
-    print '  inc_rate: 0x{:X}'.format(best[1], best[1])
-    print '  div_rate: 0x{:X}\n'.format(best[0], best[0])
+
+    print('  min_ctrl: 0x{:X}'.format(min_ctrl, min_ctrl))
+    print('  max_ctrl: 0x{:X}'.format(max_ctrl, max_ctrl))
+    print('  div_rate: 0x{:X}'.format(best[0], best[0]))
+    print('  inc_rate: 0x{:X}'.format(best[1], best[1]))
+
+    if not 2**32 > min_ctrl >= 1:
+        print_warning('\n  Warning: min_ctrl is outside the range of [1 : 2^32).')
+    if not 2**32 > max_ctrl >= 1:
+        print_warning('\n  Warning: max_ctrl is outside the range of [1 : 2^32).')
+
+    if verilog:
+        print_theme('Chirp module Verilog instantiation code:')
+        i = max(11, len(hex(min_ctrl))+2, len(hex(max_ctrl))+2, len(hex(best[0]))+2, len(hex(best[1]))+2)
+        print('  Chirp Chirp0 (')
+        print('     .clk       ( {:<{}} ),'.format('chirp_clk', i))
+        print('     .delay     ( {:<{}} ),'.format('chirp_delay', i))
+        print('     .min_ctrl  ( {:<{}} ),'.format('32\'h{:X}'.format(min_ctrl), i))
+        print('     .max_ctrl  ( {:<{}} ),'.format('32\'h{:X}'.format(max_ctrl), i))
+        print('     .div_rate  ( {:<{}} ),'.format('32\'h{:X}'.format(best[0]), i))
+        print('     .inc_rate  ( {:<{}} ),'.format('32\'h{:X}'.format(best[1]), i))
+        print('     .nco_reset ( {:<{}} ),'.format('nco_reset', i))
+        print('     .nco_ctrl  ( {:<{}} )'.format('nco_ctrl', i))
+        print('  );')
+    print()
 
 
 @cli.command()
 @click.argument('freq', type=float)
-@click.option('--clock', '-c', default=100000000)
-def nco(freq, clock):
-    print '\nNCO SPECIFICATIONS:\n'
-    print '  clk:  {} Hz'.format(clock)
-    print '  freq: {} Hz'.format(freq)
+@click.option('--clk', '-c', help='Set the clock value in Hz (default 100 MHz).', default=100000000)
+@click.option('--verilog', '-v', help='Prints out Verilog instantiation code.', is_flag=True)
+def nco_ctrl(freq, clk, verilog):
+    """
+    Calculates input port values for the NCO module.
 
-    print '\nNCO MODULE REQUIREMENTS:'
+    FREQ is the output frequency of the NCO.
+    """
 
-    control = int(round(freq * 2**32 / clock))
+    print_theme('NCO module output specifications:')
+    print('  Output frequency: {} Hz'.format(freq))
 
-    print '\033[33m'
-    print '  {} => 0x{:X} * clk / 2^32 = {}'.format(freq, control, (control * clock / 2**32))
-    print '\033[0m'
-    if 2**32 > control >= 1:
-        print '  ctrl: 0x{:X}'.format(control, control)
-    else:
-        print '  Unfortunately no control word can satisfy these specifications.'
-    print
+    print_theme('NCO module input ports:')
+
+    ctrl = int(round(freq * 2**32 / clk))
+
+    print('  clk:  {} Hz (given)'.format(clk))
+    # print_faded('  ctrl = freq * 2^32 / clk')
+    # print_faded('  ctrl of 0x{:X} will result in a freq of {}'.format(ctrl-1, (ctrl-1)*clk/2**32))
+    print('  ctrl of 0x{:X} will result in a freq of {}'.format(ctrl, ctrl * clk / 2**32))
+    # print_faded('  ctrl of 0x{:X} will result in a freq of {}'.format(ctrl+1, (ctrl+1)*clk/2**32))
+
+    if not 2**32 > ctrl >= 1:
+        print_warning('  Warning: this control word is outside the range of [1 : 2^32).')
+
+    if verilog:
+        print_theme('NCO module Verilog instantiation code:')
+        i = max(9, len(hex(ctrl))+2)
+        print('  NCO NCO0 (')
+        print('     .clk     ( {:<{}} ),'.format('nco_clk', i))
+        print('     .reset   ( {:<{}} ),'.format('nco_reset', i))
+        print('     .ctrl    ( {:<{}} ),'.format('32\'h{:X}'.format(ctrl), i))
+        print('     .sin_out ( {:<{}} ),'.format('sin_out', i))
+        print('     .cos_out ( {:<{}} )'.format('cos_out', i))
+        print('  );')
+    print()
+
+
+@cli.command()
+@click.argument('ctrl', type=BASED_INT)
+@click.option('--clk', '-c', help='Set the clock value in Hz (default 100 MHz).', default=100000000)
+def nco_freq(ctrl, clk):
+    """
+    Calculates the NCO frequency based on the input control word.
+
+    CTRL is the frequency control word to the NCO.
+    """
+
+    print_theme('NCO module input ports:')
+    print('  clk:  {} Hz'.format(clk))
+    print('  ctrl: 0x{:X}'.format(ctrl))
+
+    print_theme('NCO module output specifications:')
+    print_faded('  freq = ctrl * clk / 2^32')
+    print('  ctrl of 0x{:X} will result in a freq of {} Hz'.format(ctrl, ctrl * clk / 2**32))
 
 if __name__ == "__main__":
     cli()
